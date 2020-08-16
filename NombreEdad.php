3804421128530743c855ac93183cd89a92838988
Elías Quintanilla <69444359+Elias-Quintanilla@users.noreply.github.com>
2020-08-15 21:24:03 -0600

    Delete NombreEdad.php

--- NombreEdad.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Definición de variables
-$nombre = $_POST['nombre'];
-$edad = $_POST['edad'];
-
-//Impriendo las variables
-echo "Hola " . $nombre . "<br/>";
-echo "Tú tienes " . $edad . " años<br/>";
-?>
